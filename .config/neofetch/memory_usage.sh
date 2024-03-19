37039ab0cd3cbfded447e5cf8b321a7646e4d560
AllenReder <yiwenyou_allen@outlook.com>
2024-03-19 11:22:37 +0800

    删除 memory_usage.sh

--- .config/neofetch/memory_usage.sh
+++
@@ -1,8 +0,0 @@
-# 获取总内存和已用内存
-total=$(free | grep Mem | awk '{print $2}')
-used=$(free | grep Mem | awk '{print $3}')
-
-# 计算占用率
-rate=$(echo "scale=2; $used / $total * 100" | bc)
-
-echo "Memory Usage: $rate%"
